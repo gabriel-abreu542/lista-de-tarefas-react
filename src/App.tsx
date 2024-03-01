a37ae5b61393d98fef92743e10a3201e115cff3b
gabriel-abreu542 <120766799+gabriel-abreu542@users.noreply.github.com>
2024-03-01 15:31:38 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,24 @@ import Button from "./components/Button";
 
 function App() {
   const [isDark, setDark] = useState(true);
+  const [list, setList] = useState<string[]>([]);
+  const [newTask, setNewTask] = useState<string>("");
+  // maybe add task description later
 
+  const deleteList = () => {
+    setList([]);
+  };
+
+  const handleAddTask = () => {
+    let newTaskItem: string = newTask;
+    if (newTask != "") {
+      let updatedList = [...list];
+      updatedList.push(newTaskItem);
+      setList(updatedList);
+      setNewTask("");
+    }
+  };
+  
   return (
     <>
       <div className="App" data-theme={isDark ? "dark" : "light"}>
@@ -20,8 +37,15 @@ function App() {
           <ListGroup items={[]} heading={"To Do List"} />
         </div>
         <div className="input">
-          <TextInput id="input-task" value={""}></TextInput>
-          <Button label="Add" onClick={() => console.log("Teste")}></Button>
+          <div className="input-container">
+            <TextInput
+              id="input-task"
+              value={newTask}
+              handleChange={(e) => setNewTask(e.target.value)}
+            ></TextInput>
+            <Button label="Add" onClick={handleAddTask}></Button>
+            <Button label="Delete" onClick={deleteList}></Button>
+          </div>
         </div>
       </div>
     </>
